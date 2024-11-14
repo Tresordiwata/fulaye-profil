db1f6b5d2d22b08a7afe888a2c9723978572a380
Tresordiwata <60674875+Tresordiwata@users.noreply.github.com>
2024-11-14 01:29:44 +0100

    correction url

--- src/utils/global.jsx
+++ src/utils/global.jsx
@@ -1,5 +1,5 @@
 // export const BACKEND = "http://localhost/root/backend-fulaye/";
-export const BACKEND = "https://api.fulaye/fulaye/";
+export const BACKEND = "https://api.fulaye.com/fulaye/";
 export const API = BACKEND + "api.php";
 export const getData = async (qry, id = null) => {
 
